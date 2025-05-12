d2e566f4083614623299d43789c4d62517a6a415
GianmarioAbrioni <gianmario.abrioni90@gmail.com>
2025-05-12 15:13:31 +0200

    Add files via upload

--- public/js/staking-simple-connector.js
+++ public/js/staking-simple-connector.js
@@ -664,24 +664,3 @@ document.addEventListener('DOMContentLoaded', function() {
   
   // Run initialization
 init();
-
-// 🛡️ Fix: Aggiornamento UI sempre al cambio account, rete, disconnessione e al load pagina
-window.addEventListener('load', () => {
-    if (window.ethereum && ethereum.selectedAddress) {
-        updateUI();
-    }
-});
-
-if (window.ethereum) {
-    ethereum.on('accountsChanged', (accounts) => {
-        updateUI();
-    });
-
-    ethereum.on('chainChanged', (chainId) => {
-        updateUI();
-    });
-
-    ethereum.on('disconnect', () => {
-        updateUI();
-    });
-}

db2e7e687d3983615c4f9371414695145c27e2b8
GianmarioAbrioni <gianmario.abrioni90@gmail.com>
2025-05-19 23:18:38 +0200

    Add files via upload

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import pg from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import * as schema from "@shared/schema2.js";
+import * as schema from "@shared/schema2";
 
 /**
  * IASE Project - PostgreSQL Configuration

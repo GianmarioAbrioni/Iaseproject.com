16faaa9d78295bc98d29785b5f51519b7033c043
GianmarioAbrioni <gianmario.abrioni90@gmail.com>
2025-05-21 17:01:40 +0200

    Add files via upload

--- server/vite.ts
+++ server/vite.ts
@@ -42,29 +42,33 @@ export async function setupVite(app: Express, server: Server) {
 
   app.use(vite.middlewares);
   app.use("*", async (req, res, next) => {
-    const url = req.originalUrl;
+  const url = req.originalUrl;
 
-    try {
-      const clientTemplate = path.resolve(
-        import.meta.dirname,
-        "..",
-        "client",
-        "index.html",
-      );
+  // Se è una chiamata API, lascia passare
+  if (url.startsWith("/api")) {
+    return next();
+  }
 
-      // always reload the index.html file from disk incase it changes
-      let template = await fs.promises.readFile(clientTemplate, "utf-8");
-      template = template.replace(
-        `src="/src/main.tsx"`,
-        `src="/src/main.tsx?v=${nanoid()}"`,
-      );
-      const page = await vite.transformIndexHtml(url, template);
-      res.status(200).set({ "Content-Type": "text/html" }).end(page);
-    } catch (e) {
-      vite.ssrFixStacktrace(e as Error);
-      next(e);
-    }
-  });
+  try {
+    const clientTemplate = path.resolve(
+      import.meta.dirname,
+      "..",
+      "client",
+      "index.html"
+    );
+    let template = await fs.promises.readFile(clientTemplate, "utf-8");
+    template = template.replace(
+      `src="/src/main.tsx"`,
+      `src="/src/main.tsx?v=${nanoid()}"`
+    );
+
+    const page = await vite.transformIndexHtml(url, template);
+    res.status(200).set({ "Content-Type": "text/html" }).end(page);
+  } catch (e) {
+    vite.ssrFixStacktrace(e as Error);
+    next(e);
+  }
+});
 }
 
 export function serveStatic(app: Express) {
